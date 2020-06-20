71c05d2c682f4830fcf10a5c1315191b9e0ab777
kaushikppe <kaushik_ppe@yahoo.com>
2020-06-20 01:01:14 -0700

    added new feature

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Kanishka
+          Hello Kanishka new feature
         </a>
       </header>
     </div>
